1300406ecda9b5d9c05bdfea91f46102cda4877c
Roxanne Nasraty <rsnasraty@gmail.com>
2020-04-24 14:44:52 -0500

    completed

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,56 +1,41 @@
-const journalEntryCollection = [
-  {
-    date: "4-17-2020",
-    concepts: "Javascript Objects and Arrays",
-    entry:
-      "We just finished the first group project and are now beginning javascript.",
-    mood: "Calm"
-  },
-  {
-    date: "04/16/20",
-    concepts: "Finished Group Projects and Began Javascript",
-    entry:
-      "We all presented our Celebrity Tribute Group Projects and then started to discuss Javascript in class.",
-    mood: "Tired"
-  },
-  {
-    date: "04/15/20",
-    concepts: "Last Day of Celebrity Tribute",
-    entry:
-      "My group worked together to put together our Celebrity Tribute project. We chose David Bowie.",
-    mood: "Determined"
-  }
+const journalEntries = [
+   {
+      date: "4-17-2020",
+      concepts: "Javascript Objects and Arrays",
+      entry: "We just finished the first group project and are now beginning javascript.",
+      mood: "Calm"
+   },
+   {
+      date: "04/16/20",
+      concepts: "Finished Group Projects and Began Javascript",
+      entry: "We all presented our Celebrity Tribute Group Projects and then started to discuss Javascript in class.",
+      mood: "Tired"
+   },
+   {
+      date: "04/15/20",
+      concepts: "Last Day of Celebrity Tribute",
+      entry: "My group worked together to put together our Celebrity Tribute project. We chose David Bowie.",
+      mood: "Determined"
+   }
 ];
 
-const makeJournalEntryComponent = journalEntry => {
-  // Create your own HTML structure for a journal entry
-  return `
-   <p class="date">:${journalEntry.date}</p>
-   <p class="concept">${journalEntry.concept}</p>
-   <p class="entry">${journalEntry.entry}</p>
-   <p class="mood">Current Mood:${journalEntry.mood}</p>
- 
+const makeJournalEntry = (entry) => {
+   return `
+   <h3><b>Today's Entry</b></h3>
+   <p class="date">${entry.date}</p>
+   <p class="concepts">${entry.concepts}</p>
+   <p class="entry">${entry.entry}</p>
+   <p class="mood">Current Mood: ${entry.mood}</p>
    `
 };
 
-const entryLog = document.querySelector(".entryLog")
-
 const renderJournalEntries = (entries) => {
-   
-for (let i = 0; i < entries.length; i++) {
-    const entry = entries[i]
-    entryLog.innerHTML += makeJournalEntryComponent(entry)
-    }}
-
-
-renderJournalEntries(entries);
-
-/* const pushEntry = function(entry) {
-  journalEntryCollection.push(entry);
+   const container = document.querySelector(".entryLog");
+   entries.forEach(entry => {
+      const entryCode = makeJournalEntry(entry);
+      container.innerHTML += entryCode;
+   });
 };
 
-pushEntry(journalEntry);
-pushEntry(journalEntry2);
-pushEntry(journalEntry3);
-console.log(journalEntryCollection);
- */
+
+renderJournalEntries(journalEntries);
