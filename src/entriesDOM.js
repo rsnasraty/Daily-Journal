e48112e69b2d582664f7bd3a09fd0fd45ffc640c
Roxanne Nasraty <rsnasraty@gmail.com>
2020-05-05 16:10:06 -0500

    Finished Part 6

--- src/entriesDOM.js
+++ src/entriesDOM.js
@@ -1,7 +1,12 @@
+import makeJournalEntry from "../src/entryComponent.js"
+
+
 const renderJournalEntries = (entries) => {
-    const container = document.querySelector(".entryLog");
-    entries.forEach(entry => {
-      const entryCode = makeJournalEntry(entry);
-      container.innerHTML += entryCode;
-    });
-  };
+  const container = document.querySelector(".entryLog");
+  entries.forEach(entry => {
+    const entryCode = makeJournalEntry(entry);
+    container.innerHTML += entryCode;
+  });
+};
+
+export default renderJournalEntries
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,10 +1,4 @@
+import API from "../data.js"
+import renderJournalEntries from "../entriesDOM.js"
+
 API.fetchEntries().then(renderJournalEntries);
-
-
-/*
-    Main application logic that uses the functions and objects
-    defined in the other JavaScript files.
-
-    Change the fake variable names below to what they should be
-    to get the data and display it.
-*/
