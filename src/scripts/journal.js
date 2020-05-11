877868eefe3bae2c74a82375c5849866be9dfba8
Roxanne Nasraty <rsnasraty@gmail.com>
2020-05-11 15:13:21 -0500

    Fixed factory

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,15 +1,41 @@
 import API from "../data.js";
 import renderJournalEntries from "../entriesDOM.js";
 
-API.fetchEntries().then(renderJournalEntries);
 
-const addRecordEntryEventListener = () => {
-    const recordEntry = document.querySelector("record_Entry");
+//Factory Function to make the new entry
+const createNewEntry = (date, concepts, entry, mood) => {
+    return {
+    'date': dateInput.value,
+    'concepts': conceptsInput.value,
+    'entry': entryInput.value,
+    'mood': moodInput.value
+  }
 
-    recordEntry.addEventListener("click", () => {
-        const dateInput = document.getElementById("Date");
-        const conceptsInput = document.getElementById("Concepts");
-        const entryInput = document.getElementById("Entry");
-        const moodInput = document.getElementById("Mood");
+}
 
-      
+const recordEntry = document.querySelector("#record_Entry");
+//get record Entry button from index
+
+
+recordEntry.addEventListener("click", () => {
+
+const dateInput = document.querySelector("#Date");
+const conceptsInput = document.querySelector("#Concepts");
+const entryInput = document.querySelector("#Entry");
+const moodInput = document.querySelector("#Mood");
+
+
+//calls upon the factory function here
+const newEntry = createNewEntry(dateInput.value, conceptsInput.value, entryInput.value, moodInput.value)
+
+
+API.saveJournalEntry(newEntry)
+   .then(API.fetchEntries)
+   .then(renderJournalEntries);
+})
+
+
+
+
+API.fetchEntries().then(renderJournalEntries); 
+
